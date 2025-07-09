ddbc4639d9ba5fca246cd0eb1d52d10bbf78e3ce
iamAbhishekkumar <abhisonuk123@gmail.com>
2025-07-10 00:23:02 +0530

    removed listen&serve

--- functions/hello-world/hello-world.go
+++ functions/hello-world/hello-world.go
@@ -1,4 +1,4 @@
-package handler
+package main
 
 import (
 	"encoding/json"
@@ -9,8 +9,9 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	resp := Response{Message: "Hello, World from Go (Netlify)!"}
+// Exported handler function expected by Netlify
+func Handler(w http.ResponseWriter, r *http.Request) {
+	resp := Response{Message: "Hello, World from Go on Netlify!"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
 }
